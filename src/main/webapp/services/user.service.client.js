9adc14989cb074be0a3927e8da48b60ed0db4f15
Namuhan Muschin <namuhan@Namuhans-MacBook-Pro.local>
2021-02-10 19:50:57 -0800

    Added user service page

--- src/main/webapp/services/user.service.client.js
+++ src/main/webapp/services/user.service.client.js
@@ -1,11 +1,12 @@
 function UserServiceClient() {
   this.createUser = createUser;
   this.findAllUsers = findAllUsers;
-  this.findUserById = findUserById;
+//  this.findUserById = findUserById;
   this.deleteUser = deleteUser;
   this.updateUser = updateUser;
   this.url = 'https://wbdv-generic-server.herokuapp.com/api/001400298/users';
   var self = this;
+
   function createUser(user) {
     return fetch(self.url, {
       method: 'POST',
@@ -17,15 +18,18 @@ function UserServiceClient() {
       return response.json()
     })
   }
+
   function findAllUsers() {
     return fetch(self.url)
       .then(function (response) {
         return response.json()
     })
   }
-  function findUserById(userId) {
 
-  }
+//  function findUserById(userId) {
+//
+//  }
+
   function updateUser(userId, user) {
     return fetch(`${self.url}/${userId}`, {
       method: 'PUT',
@@ -35,6 +39,7 @@ function UserServiceClient() {
       body: JSON.stringify(user)
     }).then(response => response.json())
   }
+
   function deleteUser(userId) {
     return fetch(`${self.url}/${userId}`,
       {method: 'DELETE'})
