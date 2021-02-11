9763cb284728b26c002531a8b357cea6c7b5b77f
Namuhan Muschin <namuhan@Namuhans-MacBook-Pro.local>
2021-02-10 20:24:55 -0800

    Changed password to be invisible

--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -64,7 +64,7 @@ function renderUsers(users) {
       .append(`
     <tr>
         <td>${user.username}</td>
-        <td>${user.password}</td>
+        <td>*******</td>
         <td>${user.firstName}</td>
         <td>${user.lastName}</td>
         <td>${user.role}</td>
