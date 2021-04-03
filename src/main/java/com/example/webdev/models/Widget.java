e16eecdbefde1ef946eeb21dc611b6d9a71a06cd
Namuhan Muschin <namuhan@Namuhans-MacBook-Pro.local>
2021-04-02 20:53:51 -0700

    Added connection to Database and repositories

--- src/main/java/com/example/webdev/models/Widget.java
+++ src/main/java/com/example/webdev/models/Widget.java
@@ -1,11 +1,63 @@
 package com.example.webdev.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="widgets")
 public class Widget {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String topicIc;
+
+    private String name;
+    private String topicId;
     private String type;
-    private Integer size;
+    private Integer ordered;
     private String text;
+    private String src;
+    private Integer size;
+    private Integer width;
+    private Integer height;
+    private String cssClass;
+    private String style;
+    private String value;
+
+    public Widget() {
+    }
+
+    public Widget(Long id, String topicId, String type, Integer size, String text) {
+        this.id = id;
+        this.topicId = topicId;
+        this.type = type;
+        this.size = size;
+        this.text = text;
+    }
+
+//    public Widget(String name, Long id, String topicId, String type, Integer ordered, String text,
+//                  String src, Integer size, Integer width, Integer height, String cssClass, String style, String value) {
+//        this.name = name;
+//        this.id = id;
+//        this.topicId = topicId;
+//        this.type = type;
+//        this.ordered = ordered;
+//        this.text = text;
+//        this.src = src;
+//        this.size = size;
+//        this.width = width;
+//        this.height = height;
+//        this.cssClass = cssClass;
+//        this.style = style;
+//        this.value = value;
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public Long getId() {
         return id;
@@ -15,12 +67,12 @@ public class Widget {
         this.id = id;
     }
 
-    public String getTopicIc() {
-        return topicIc;
+    public String getTopicId() {
+        return topicId;
     }
 
-    public void setTopicIc(String topicIc) {
-        this.topicIc = topicIc;
+    public void setTopicId(String topicId) {
+        this.topicId = topicId;
     }
 
     public String getType() {
@@ -31,12 +83,12 @@ public class Widget {
         this.type = type;
     }
 
-    public Integer getSize() {
-        return size;
+    public Integer getOrdered() {
+        return ordered;
     }
 
-    public void setSize(Integer size) {
-        this.size = size;
+    public void setOrdered(Integer ordered) {
+        this.ordered = ordered;
     }
 
     public String getText() {
@@ -47,14 +99,59 @@ public class Widget {
         this.text = text;
     }
 
-    public Widget(Long id, String topicIc, String type, Integer size, String text) {
-        this.id = id;
-        this.topicIc = topicIc;
-        this.type = type;
-        this.size = size;
-        this.text = text;
+    public String getSrc() {
+        return src;
     }
 
-    public Widget() {
+    public void setSrc(String src) {
+        this.src = src;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public String getCssClass() {
+        return cssClass;
+    }
+
+    public void setCssClass(String cssClass) {
+        this.cssClass = cssClass;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
     }
 }
--- src/main/java/com/example/webdev/services/WidgetService.java
+++ src/main/java/com/example/webdev/services/WidgetService.java
@@ -1,6 +1,8 @@
 package com.example.webdev.services;
 
 import com.example.webdev.models.Widget;
+import com.example.webdev.repositories.WidgetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,60 +11,95 @@ import java.util.List;
 
 @Service
 public class WidgetService {
-    private List<Widget> widgets = new ArrayList<Widget>();
-    {
-        Widget w1 = new Widget(123l, "ABC123", "HEADING", 1, "Widgets for Topic ABC123");
-        Widget w2 = new Widget(234l, "ABC123", "PARAGRAPH", 1, "Lorem Ipsum");
-        Widget w3 = new Widget(345l, "ABC234", "HEADING", 2, "Widgets for Topic ABC234");
-        Widget w4 = new Widget(456l, "ABC234", "PARAGRAPH", 1, "Lorem Ipsum");
-        Widget w5 = new Widget(567l, "ABC234", "PARAGRAPH", 1, "Lorem Ipsum");
 
-        widgets.add(w1);
-        widgets.add(w2);
-        widgets.add(w3);
-        widgets.add(w4);
-        widgets.add(w5);
-    }
+    @Autowired
+    WidgetRepository repository;
+
+//    private List<Widget> widgets = new ArrayList<Widget>();
+//    {
+//
+//        Widget w1 = new Widget(123l, "ABC123", "HEADING", 1, "Widgets for Topic ABC123");
+//        Widget w2 = new Widget(234l, "ABC123", "PARAGRAPH", 1, "Lorem Ipsum");
+//        Widget w3 = new Widget(345l, "ABC234", "HEADING", 2, "Widgets for Topic ABC234");
+//        Widget w4 = new Widget(456l, "ABC234", "PARAGRAPH", 1, "Lorem Ipsum");
+//        Widget w5 = new Widget(567l, "ABC234", "PARAGRAPH", 1, "Lorem Ipsum");
+//
+//        widgets.add(w1);
+//        widgets.add(w2);
+//        widgets.add(w3);
+//        widgets.add(w4);
+//        widgets.add(w5);
+//    }
     
     public Widget createWidgetForTopic(String topicId, Widget widget) {
-        widget.setTopicIc(topicId);
-        widget.setId((new Date()).getTime());
-        widgets.add(widget);
-        return widget;
+        widget.setTopicId(topicId);
+        return repository.save(widget);
+//        widget.setId((new Date()).getTime());
+//        widgets.add(widget);
+//        return widget;
     }
     
     public List<Widget> findAllWidgets() {
-        return widgets;
+        return (List<Widget>) repository.findAll();
+//        return widgets;
     }
+
     public List<Widget> findWidgetsForTopic(String topicId) {
-        List<Widget> ws = new ArrayList<Widget>();
-        for(Widget w: widgets) {
-            if(w.getTopicIc().equals(topicId)) {
-                ws.add(w);
-            }
-        }
-        return ws;
+        return repository.findWidgetsForTopic(topicId);
+//        List<Widget> ws = new ArrayList<Widget>();
+//        for(Widget w: widgets) {
+//            if(w.getTopicId().equals(topicId)) {
+//                ws.add(w);
+//            }
+//        }
+//        return ws;
     }
 
     public Integer deleteWidget(Long id) {
-        int index = -1;
-        for(int i=0; i<widgets.size(); i++) {
-            if(widgets.get(i).getId().equals(id)) {
-                index = i;
-                widgets.remove(index);
-                return 1;
-            }
-        }
+        repository.deleteById(id);
+//        int index = -1;
+//        for(int i=0; i<widgets.size(); i++) {
+//            if(widgets.get(i).getId().equals(id)) {
+//                index = i;
+//                widgets.remove(index);
+//                return 1;
+//            }
+//        }
         return -1;
     }
 
     public Integer updateWidget(Long id, Widget widget) {
-        for(int i=0; i<widgets.size(); i++) {
-            if(widgets.get(i).getId().equals(id)) {
-                widgets.set(i, widget);
-                return 1;
-            }
-        }
-        return -1;
+        Widget originalWidget = repository.findById(id).get();
+//        Boolean isPresent = repository.findById(id).isPresent();
+//        if (isPresent) {
+//            Widget originalWidget1 = repository.findById(id).get();
+//        }
+
+        // TODO: copy all the other fields testing for null
+//        originalWidget.setText(widget.getText());
+        if (widget.getText() != null) originalWidget.setText(widget.getText());
+        if (widget.getId() != null) originalWidget.setId(widget.getId());
+        if (widget.getName() != null) originalWidget.setName(widget.getName());
+        if (widget.getTopicId() != null) originalWidget.setTopicId(widget.getTopicId());
+        if (widget.getType() != null) originalWidget.setType(widget.getType());
+        if (widget.getOrdered() != null) originalWidget.setOrdered(widget.getOrdered());
+        if (widget.getSrc() != null) originalWidget.setSrc(widget.getSrc());
+        if (widget.getSize() != null) originalWidget.setSize(widget.getSize());
+        if (widget.getWidth() != null) originalWidget.setWidth(widget.getWidth());
+        if (widget.getHeight() != null) originalWidget.setHeight(widget.getHeight());
+        if (widget.getCssClass() != null) originalWidget.setCssClass(widget.getCssClass());
+        if (widget.getStyle() != null) originalWidget.setStyle(widget.getStyle());
+        if (widget.getValue() != null) originalWidget.setValue(widget.getValue());
+
+        repository.save(originalWidget);
+
+        return 1;
+//        for(int i=0; i<widgets.size(); i++) {
+//            if(widgets.get(i).getId().equals(id)) {
+//                widgets.set(i, widget);
+//                return 1;
+//            }
+//        }
+//        return -1;
     }
 }
